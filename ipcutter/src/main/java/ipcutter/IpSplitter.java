9e689eb6f36082cfc3fb06a85eef7c5adb28cea7
Shehab Mahmoud <42918713+shehab2@users.noreply.github.com>
2021-06-20 20:35:02 +0200

    Ip_Cutter_lab (day2)

---
+++ ipcutter/src/main/java/ipcutter/IpSplitter.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ipcutter;
+
+/**
+ *
+ * @author shehab
+ */
+public class IpSplitter {
+    private String ip;
+
+    public IpSplitter(String ip) {
+        this.ip = ip;
+    }
+
+    public String getIp() {
+        return ip;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+    /** 
+     * @return integer array of ip parts.
+     */
+    public int [] splitIP(){
+        String [] parts = ip.split("\\.");
+        int [] partsInt = new int[parts.length];
+        for(int i=0;i<parts.length;i++){
+            partsInt[i] = Integer.parseInt(parts[i]);
+        }
+        return partsInt;
+    }
+    /**
+     * prints the values of the int array of ip.
+     * @param p set to true in case of print.
+     */
+    public void splitIP(boolean p){
+        int[] partsInt = splitIP();
+        if(p == true){
+            for(int val:partsInt){
+                System.out.println(val);
+            }
+        }
+    }
+}
---
+++ ipcutter/src/main/java/ipcutter/main.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ipcutter;
+
+/**
+ *
+ * @author shehab
+ */
+class main {
+    public static void main (String[] args){
+        IpSplitter s = new IpSplitter("192.168.1.1");
+        int [] x = s.splitIP();
+        for (int val:x){
+            System.out.println(val);
+        }
+        s.splitIP(true);   
+    }
+    
+}
