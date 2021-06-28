e531885111b005c50dcef95bc421897bd6571c4f
Shehab Mahmoud <42918713+shehab2@users.noreply.github.com>
2021-06-28 20:02:51 +0200

    lambdaDemo

--- lambdaDemo/src/main/java/lambdademo/StringUtils.java
+++ lambdaDemo/src/main/java/lambdademo/StringUtils.java
@@ -12,7 +12,9 @@ import java.util.function.BiPredicate;
  */
 public class StringUtils {
     public static String betterString(String s1,String s2,BiPredicate<String,String> p){
-        if(p.test(s1, s2)){
+        
+        boolean test = p.test(s1,s2);
+        if(test){
             return s1;
         }
         return s2;
--- lambdaDemo/src/main/java/lambdademo/main.java
+++ lambdaDemo/src/main/java/lambdademo/main.java
@@ -19,6 +19,7 @@ public class main {
         String longer = StringUtils.betterString(string1, string2, (s1, s2) -> s1.length() > s2.length());
         String first = StringUtils.betterString(string1, string2, (s1, s2) -> true);
         
+        
         System.out.println(longer);
         System.out.println(first);
     }
