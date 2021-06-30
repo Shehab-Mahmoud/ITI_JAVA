9363a22a8be39d6a1011af4ee9af1aad7456e606
Shehab Mahmoud <42918713+shehab2@users.noreply.github.com>
2021-06-30 07:17:39 +0200

    CitiesCountries

---
+++ lambdaTest/src/main/java/lambdatest/Greeting.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lambdatest;
+
+/**
+ *
+ * @author shehab
+ */
+@FunctionalInterface
+public interface Greeting {
+    public void preform();
+}
