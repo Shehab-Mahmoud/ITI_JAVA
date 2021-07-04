98f50881d57919e035e2ec181e0bd97b25445938
Shehab Mahmoud <42918713+shehab2@users.noreply.github.com>
2021-07-04 05:47:26 +0200

    TitanicData Update

--- TitanicData/src/main/java/titanicdata/main.java
+++ TitanicData/src/main/java/titanicdata/main.java
@@ -4,9 +4,16 @@
  * and open the template in the editor.
  */
 package titanicdata;
+
 import java.io.IOException;
 import java.util.List;
 import joinery.DataFrame;
+import tech.tablesaw.api.Table;
+import tech.tablesaw.columns.Column;
+
+import tech.tablesaw.io.csv.CsvReadOptions;
+
+
 /**
  *
  * @author shehab
@@ -15,7 +22,8 @@ public class main {
     public static void main(String[] args) throws IOException{
         
         System.out.println("Using joinery : \n ---------------------------------");
-        DataFrame<Object> df = DataFrame.readCsv("D:\\iti\\Java\\day5\\Data_to_use\\titanic.csv");
+        String path = "D:\\iti\\Java\\day5\\Data_to_use\\titanic.csv";
+        DataFrame<Object> df = DataFrame.readCsv(path);
         
         DataFrame<Object> df1 = df.retain("survived","age","fare")
                 .groupBy(s -> s.get(0))
@@ -37,5 +45,16 @@ public class main {
         DataFrame<Object> df4 = df.retain("fare")
                 .stddev();
         System.out.println("4. Fare standard diviation : "+df4.get(0,0));
+        
+        // using tablesaw
+        System.out.println("\n Using TableSaw \n -------------------------");
+        
+        Table titanicData;
+        titanicData = Table.read().csv(path);
+        List<String> colNames = titanicData.columnNames();
+        List<Column<?>> dataStructure = titanicData.columns();
+        System.out.println("1.Column structure : \n"+dataStructure);
+        System.out.println("2.Full Structure:\n"+titanicData.structure());
+        System.out.println("3.Summary statstics : \n"+titanicData.summary());
     }
 }
